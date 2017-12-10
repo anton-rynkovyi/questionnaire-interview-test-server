0a08eb4dcd3562d07b7b3bf24a817f01c8ea54f0
Anton Rynkovoy <anton.rynkovoy@gmail.com>
2017-12-10 17:54:04 +0200

    first commit. create user pojo.

---
+++ source/com/qit/models/User.java
@@ -0,0 +1,42 @@
+package com.qit.models;
+
+public class User {
+
+    String username;
+    String password;
+    boolean enabled;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", enabled=" + enabled +
+                '}';
+    }
+}
