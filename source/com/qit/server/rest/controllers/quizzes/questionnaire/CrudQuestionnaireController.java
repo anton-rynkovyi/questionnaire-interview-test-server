c50f570f62aa046964a4f91749e92cf58bfc463c
Anton Rynkovoy <anton.rynkovoy@gmail.com>
2018-01-22 19:53:34 +0200

    add save questions to controller

--- source/com/qit/server/rest/controllers/quizzes/questionnaire/CrudQuestionnaireController.java
+++ source/com/qit/server/rest/controllers/quizzes/questionnaire/CrudQuestionnaireController.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/questionnaires")
+@RequestMapping("/api/questionnaires")
 public class CrudQuestionnaireController {
 
     @Autowired
